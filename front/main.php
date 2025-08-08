7428b012baf0745ff75be59d6d780f271919d513
rita <qazasd1011@gmail.com>
2025-08-08 10:45:55 +0800

    main/news

--- front/main.php
+++ front/main.php
@@ -32,6 +32,18 @@ function ww() {
     <span class="t botli">最新消息區
     </span>
     <ul class="ssaa" style="list-style-type:decimal;">
+        <?php
+        $news=$News->all(['sh'=>1]);
+        foreach($news as $new){
+            echo "<li>";
+            echo mb_substr($new['text'],0,10);
+            echo "<div class='all' style='display:none;'>";
+            echo $new['text'];
+            echo "</div>";
+            echo "</li>";
+        }
+        ?>
+        
     </ul>
     <div id="altt"
         style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
